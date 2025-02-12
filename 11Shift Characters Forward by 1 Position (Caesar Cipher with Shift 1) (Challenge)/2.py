295b80e490299d8e0c5a11a75e3ca6b3ad7bac22
CCT64 <chirachortkultorn@gmail.com>
2025-02-12 04:18:32 +0000

    asdb

---
+++ 11Shift Characters Forward by 1 Position (Caesar Cipher with Shift 1) (Challenge)/2.py
@@ -0,0 +1,8 @@
+text = input("")
+rev = ""
+
+for i in range(len(text) - 1, -1,-1):
+    rev = rev + text[i]
+
+print(rev)
+
---
+++ 3.py
@@ -0,0 +1,11 @@
+x = input("")
+new_str = ""
+
+for i in x:
+    if i != " ":
+        new_str = new_str + i
+
+print(new_str)
+
+
+
